09bdd3c7517d05fc0c1be9a0e22c35dc77d2a9ea
Ally Mrisho <mrishoally35@gmail.com>
2024-02-24 08:58:43 +0300

    User_input.py

---
+++ User_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = input("How old are you?")
+location = input("Where are you came from?")
+message = f"Hello {name}! It's great to know you're {age} Years old and from {location}."
+print(message)
